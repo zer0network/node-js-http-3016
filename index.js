b206eeb457b5e2a685e500f2d5dd39f85f124b7a
Azumabashi <hiro_kataoka@hkataoka.local>
2022-04-11 15:41:20 +0900

    refactor

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const pug = require('pug');
 const server = http
   .createServer((req, res) => {
     const now = new Date();
-    console.info('[' + now + '] Requested by ' + req.socket.remoteAddress);
+    console.info(`[${now}] Requested by ${req.socket.remoteAddress}`);
     res.writeHead(200, {
       'Content-Type': 'text/html; charset=utf-8'
     });
@@ -43,13 +43,10 @@ const server = http
             rawData += chunk;
           })
           .on('end', () => {
-            const qs = require('querystring');
-            const answer = qs.parse(rawData);
-            const body = answer['name'] + 'さんは' +
-              answer['favorite'] + 'に投票しました';
-            console.info('[' + now + '] ' + body);
-            res.write('<!DOCTYPE html><html lang="ja"><body><h1>' +
-              body + '</h1></body></html>');
+            const answer = new URLSearchParams(rawData);
+            const body = `${answer.get('name')}さんは${answer.get('favorite')}に投票しました`;
+            console.info(`[${now}] ${body}`);
+            res.write(`<!DOCTYPE html><html lang="ja"><body><h1>${body}</h1></body></html>`);
             res.end();
           });
         break;
@@ -58,12 +55,12 @@ const server = http
     }
   })
   .on('error', e => {
-    console.error('[' + new Date() + '] Server Error', e);
+    console.error(`[${new Date()}] Server Error`, e);
   })
   .on('clientError', e => {
-    console.error('[' + new Date() + '] Client Error', e);
+    console.error(`[${new Date()}] Client Error`, e);
   });
 const port = 8000;
 server.listen(port, () => {
-  console.info('[' + new Date() + '] Listening on ' + port);
+  console.info(`[${new Date()}] Listening on ${port}`);
 });
